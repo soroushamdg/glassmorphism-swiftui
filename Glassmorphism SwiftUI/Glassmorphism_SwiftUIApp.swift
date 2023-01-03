57b916746b301955c0bdde77229e29d7355c340f
soroushamdg <soroushmpour@gmail.com>
2023-01-03 10:11:03 -0500

    Initial Commit

---
+++ Glassmorphism SwiftUI/Glassmorphism_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glassmorphism_SwiftUIApp.swift
+//  Glassmorphism SwiftUI
+//
+//  Created by Soro on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct Glassmorphism_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
